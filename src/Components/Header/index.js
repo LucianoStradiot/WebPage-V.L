2fd29a9aed723f1af1df8b50734ba384df2bc366
unknown <lstradiot15@gmail.com>
2023-11-10 15:12:28 -0300

    Added folders and files to start to work

---
+++ src/Components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return <Link to="/">Header</Link>;
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Routes from './Routes';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
