b812e90794d9f41721c5e94f9ad1fdd0632af4af
unknown <lstradiot15@gmail.com>
2024-01-10 15:16:00 -0300

    Reorder the views and finished the photo an description for now on the home view

--- src/Views/Profesionales/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './profesionales.module.css';
-
-const Profesionales = () => {
-  return <div className={styles.container}>Blog y Recetas</div>;
-};
-
-export default Profesionales;
--- src/Views/SuperAdmin/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './superAdmin.module.css';
-
-function SuperAdmin() {
-  return <div className={styles.container}>SuperAdmin</div>;
-}
-
-export default SuperAdmin;
